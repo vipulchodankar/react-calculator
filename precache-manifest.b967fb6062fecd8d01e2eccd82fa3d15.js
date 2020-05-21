8eca2188afcfdc3a6211dd03b38ae65d68785c84
Vipul Chodankar <vipul72c@gmail.com>
2020-05-21 23:32:08 +0530

    Updates

---
+++ precache-manifest.b967fb6062fecd8d01e2eccd82fa3d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afed78f0bb34dcd2230f1a971aaf9d6",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e23049008b89d881bf2f",
+    "url": "/react-calculator/static/css/main.26a77f31.chunk.css"
+  },
+  {
+    "revision": "84cb8cec3d331e8b6328",
+    "url": "/react-calculator/static/js/2.60438ce5.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.60438ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23049008b89d881bf2f",
+    "url": "/react-calculator/static/js/main.85cbfab3.chunk.js"
+  },
+  {
+    "revision": "bc9df7f23adb22dec68c",
+    "url": "/react-calculator/static/js/runtime-main.7179f7b2.js"
+  }
+]);
